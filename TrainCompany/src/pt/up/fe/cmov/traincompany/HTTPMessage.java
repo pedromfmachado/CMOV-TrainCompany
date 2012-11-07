b654c7fc031f569ed3c2f8c048b7c5d37a753fa3
Pedro Machado <pedromfmachado@gmail.com>
2012-11-07 01:40:40 +0000

    post implemented. Problem with percent-encoding, needs to be changed to use httpconnection instead of httppost

--- TrainCompany/src/pt/up/fe/cmov/traincompany/HTTPMessage.java
+++ TrainCompany/src/pt/up/fe/cmov/traincompany/HTTPMessage.java
@@ -1,6 +1,9 @@
 package pt.up.fe.cmov.traincompany;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,15 +13,17 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import android.util.Log;
+
 public class HTTPMessage {
 	
-	public static HttpResponse POST(String server, HashMap<String, String> values){
+	public static String POST(String server, HashMap<String, String> values){
 		
+			String result = "";
 		    // Create a new HttpClient and Post Header
 		    HttpClient httpclient = new DefaultHttpClient();
 		    HttpPost httppost = new HttpPost("http://www.yoursite.com/script.php");
@@ -27,14 +32,28 @@ public class HTTPMessage {
 		        // Add your data
 		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
 		        
-		        for(String key: values.keySet())
+		        for(String key: values.keySet()){
+		        	
 		        	nameValuePairs.add(new BasicNameValuePair(key, values.get(key)));
+		        	
+		        }
 		        
-		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs,"utf-8"));
 
 		        // Execute HTTP Post Request
 		        HttpResponse response = httpclient.execute(httppost);
-		        return response;
+		        
+		        InputStream in = response.getEntity().getContent();
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+	            StringBuilder str = new StringBuilder();
+	            String line = null;
+	            while((line = reader.readLine()) != null){
+	                str.append(line + "\n");
+	            }
+	            in.close();
+	            result = str.toString();
+	            
+	        	Log.w("key + value", result);
 		        
 		    } catch (ClientProtocolException e) {
 		        // TODO Auto-generated catch block
@@ -42,7 +61,7 @@ public class HTTPMessage {
 		        // TODO Auto-generated catch block
 		    }
 		    
-		    return null;
+		    return result;
 	}
 
 }
--- TrainCompany/src/pt/up/fe/cmov/traincompany/LoginActivity.java
+++ TrainCompany/src/pt/up/fe/cmov/traincompany/LoginActivity.java
@@ -1,8 +1,15 @@
 package pt.up.fe.cmov.traincompany;
 
+import java.util.HashMap;
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -11,7 +18,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         
-        find
+        findViewById(R.id.login_button).setOnClickListener(loginListener);
     }
 
     @Override
@@ -19,4 +26,26 @@ public class LoginActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_login, menu);
         return true;
     }
+    
+    private OnClickListener loginListener = new OnClickListener() {
+    	
+		public void onClick(View arg0) {
+			
+			String email = ((EditText)findViewById(R.id.email_edit)).getText().toString();
+			String password = ((EditText)findViewById(R.id.password_edit)).getText().toString();
+			
+			if(email.trim().length() != 0 && password.trim().length() != 0){
+				HashMap<String, String> values = new HashMap<String, String>(2);
+				values.put("[user_login][email]", email);
+				values.put("[user_login][password]", password);
+				
+				String response = HTTPMessage.POST(R.string.server_address + "/users/sign_in.json", values);
+				
+				Toast.makeText(LoginActivity.this, response, Toast.LENGTH_LONG).show();
+			}
+			else
+				Toast.makeText(LoginActivity.this, "Please fill in the email and password", Toast.LENGTH_LONG).show();
+		
+		}
+	};
 }
