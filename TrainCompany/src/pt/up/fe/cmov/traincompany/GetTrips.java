99bb88bdbbc33b7419d6b2cada90905fb03afe3f
Pedro Machado <pedromfmachado@gmail.com>
2012-11-11 16:09:06 +0000

    making reservations

--- TrainCompany/src/pt/up/fe/cmov/traincompany/GetTrips.java
+++ TrainCompany/src/pt/up/fe/cmov/traincompany/GetTrips.java
@@ -8,73 +8,75 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import Requests.AsyncGet;
+import Requests.AsyncPost;
 import Requests.ResponseCommand;
 import Requests.ResponseCommand.ERROR_TYPE;
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class GetTrips extends Activity {
-	
+
 	Bundle bundle;
 	ProgressDialog loading;
-	
+
 	ArrayList<String> names = new ArrayList<String>();
 	ArrayList<String> descriptions = new ArrayList<String>();
 	ArrayList<String> ids = new ArrayList<String>();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.get_trips);
-        
-        bundle = getIntent().getExtras();
-        
-        populate();
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.get_trips);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_get_trips, menu);
-        return true;
-    }
-    
-    public void populate(){
-    	
-    	String server = getString(R.string.server_address) + "reservations/get_trips";
-    	
-    	/*
-    	 * intent.putExtra("arrival_id", ids.get(arrival_pos));
-		intent.putExtra("departure_id", ids.get(departure_pos));
-		intent.putExtra("arrival_name", names.get(arrival_pos));
-		intent.putExtra("departure_name", names.get(departure_pos));
-		intent.putExtra("date", date);
-		intent.putExtra("time", time);
-    	 * 
-    	 */
-    	
-    	String time = bundle.getString("time");
-    	String date = bundle.getString("date");
-    	String arrival_id = bundle.getString("arrival_id");
-    	String departure_id = bundle.getString("departure_id");
-    	
-    	HashMap<String, String> values = new HashMap<String, String>();
-    	values.put("date", date);
-    	values.put("arrivalStation_id", arrival_id);
-    	values.put("departureStation_id", departure_id);
-    	values.put("time", time);
-    	
-    	Log.i("trips", date + " - " + arrival_id + " - " + departure_id + " - " + time);
-    	
-    	if(values.containsValue("")){
+		bundle = getIntent().getExtras();
+
+		populate();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_get_trips, menu);
+		return true;
+	}
+
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.btMakeReservation:
+			makeReservation();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public void populate(){
+
+		String server = getString(R.string.server_address) + "reservations/get_trips";
+
+		String time = bundle.getString("time");
+		String date = bundle.getString("date");
+		String arrival_id = bundle.getString("arrival_id");
+		String departure_id = bundle.getString("departure_id");
+
+		HashMap<String, String> values = new HashMap<String, String>();
+		values.put("date", date);
+		values.put("arrivalStation_id", arrival_id);
+		values.put("departureStation_id", departure_id);
+		values.put("time", time);
+
+		Log.i("trips", date + " - " + arrival_id + " - " + departure_id + " - " + time);
+
+		if(values.containsValue("")){
 
 			Toast.makeText(GetTrips.this, "Every field must be filled", Toast.LENGTH_LONG).show();
 			return;
@@ -93,31 +95,31 @@ public class GetTrips extends Activity {
 				}
 
 				try{
-					
+
 					JSONArray json = new JSONArray((String)results[0]);
 
-					
+
 					for(int i = 0; i < json.length(); i++){
-						
+
 						String departure = json.getJSONObject(i).getString("departure");
 						String arrival = json.getJSONObject(i).getString("arrival");
 						String time = json.getJSONObject(i).getString("time");
-						
+
 						JSONObject trip = json.getJSONObject(i).getJSONObject("trip");
 
 						names.add(departure + " - " + arrival);
 						ids.add(trip.getString("id"));
 						descriptions.add(time);
 					}
-					
+
 					ListAdapter adapter = new ListAdapter(GetTrips.this, names, descriptions);
 
 					ListView list = (ListView) findViewById(R.id.list);
 					list.setAdapter(adapter);
-					
+
 				}
 				catch(JSONException e){
-					
+
 					e.printStackTrace();
 				}
 
@@ -132,5 +134,81 @@ public class GetTrips extends Activity {
 
 			}
 		}).execute();
-    }
+	}
+
+	public void makeReservation(){
+		
+		String server = getString(R.string.server_address)+"reservations";
+
+		String departure_id = bundle.getString("departure_id");
+		String arrival_id = bundle.getString("arrival_id");
+		String time = bundle.getString("time");
+		String date = bundle.getString("date");
+
+		HashMap<String, String> values = new HashMap<String, String>(2);
+		values.put("[reservation][departureStation_id]", departure_id.trim());
+		values.put("[reservation][arrivalStation_id]", arrival_id.trim());
+		values.put("[reservation][date]", date.trim());
+		values.put("time", time.trim());
+
+		if(values.containsValue("")){
+			Toast.makeText(GetTrips.this, "Please fill in the email and password", Toast.LENGTH_LONG).show();
+			return;
+		}
+		loading = ProgressDialog.show(GetTrips.this, "", "Making Reservation");
+		new AsyncPost(server, values, new ResponseCommand() {
+
+			public void onError(ERROR_TYPE error) {
+
+				loading.dismiss();
+				Toast.makeText(GetTrips.this, "Server Error", Toast.LENGTH_LONG).show();
+			}
+
+			public void onResultReceived(Object... results) {
+				if(results.length > 0){
+
+					if(results[0] == null || ((String)results[0]).equals("")){
+
+						loading.dismiss();
+						Toast.makeText(GetTrips.this, "Connections problems, verify your network signal", Toast.LENGTH_LONG).show();
+						return;
+					}
+
+					JSONObject json;
+					try {
+
+						json = new JSONObject((String)results[0]);
+
+						boolean success = json.optBoolean("success");
+						if(success){
+
+							/*Integer user_id = json.getInt("id");
+							String name = json.getString("name");
+							String email = json.getString("email");
+							String token = json.getString("auth_token");*/
+
+							//Global.datasource.createReservation(Reservation_id, uuid, User_id, Trip_id, departureStation_id, arrivalStation_id)
+
+							Toast.makeText(GetTrips.this, "Reservation created successfully!", Toast.LENGTH_LONG).show();
+
+
+						}
+						else{
+
+							loading.dismiss();
+							Toast.makeText(GetTrips.this, "Error creating reservation", Toast.LENGTH_LONG).show();
+						}
+
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+
+				loading.dismiss();
+			}
+
+
+		}).execute();
+	}
 }
