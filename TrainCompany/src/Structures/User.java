df681a1dc219ca9f9349830bbd539ce44869bcbe
Pedro Machado <pedromfmachado@gmail.com>
2012-11-15 02:58:46 +0000

    minor corrections

--- TrainCompany/src/Structures/User.java
+++ TrainCompany/src/Structures/User.java
@@ -103,12 +103,12 @@ public class User extends Structure {
 					}
 				}
 				
-				if(errors.size() == 0 && !u.role.equals(Global.ADMIN)){
+				if(u != null && errors.size() == 0 && !u.role.equals(Global.ADMIN)){
 					
 					Intent i = new Intent(activity, MainMenu.class);
 					activity.startActivity(i);
 				}
-				else if(u.role.equals(Global.ADMIN)){
+				else if(u != null && u.role.equals(Global.ADMIN)){
 					
 					errors.add("This application is not to be used by admins");
 				}
